433fe10bf5bb7a2a19c8963093dea43ae510258a
@zonarolo <luis.rolo@bootcamp-upgrade.com>
2020-06-22 21:25:15 +0200

    Dice-game

---
+++ script.js
@@ -0,0 +1,22 @@
+function game() {
+  let randomNumber1 = Math.floor(Math.random() * 6 + 1);
+  let randomNumber2 = Math.floor(Math.random() * 6 + 1);
+  document
+    .querySelector(".img1")
+    .setAttribute("src", `images/dice${randomNumber1}.png`);
+
+  document
+    .querySelector(".img2")
+    .setAttribute("src", `images/dice${randomNumber2}.png`);
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+  } else {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
+  }
+  if (randomNumber1 == randomNumber2) {
+    document.querySelector("h1").innerText = "Draw!";
+  }
+}
+
+document.addEventListener("click", game);
